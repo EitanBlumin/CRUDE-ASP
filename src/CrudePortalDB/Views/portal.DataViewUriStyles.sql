17a2c6354401511e84b3dd1decc26f51b5cfc5c3
eitan.blumin <eitan@madeiradata.com>
2018-07-26 17:08:41 +0300

    added dataview uri button styles

--- src/CrudePortalDB/Views/portal.DataViewUriStyles.sql
+++ src/CrudePortalDB/Views/portal.DataViewUriStyles.sql
@@ -2,6 +2,11 @@
 AS
 SELECT *
 FROM (VALUES
- (1, 'Link', 'fa fa-link', CONVERT(bit, 1))
-,(2, 'Button', 'fa fa-square', CONVERT(bit, 0))
-) AS V(StyleValue, StyleLabel, StyleGlyph, StyleDefault)
+ (1, 'Link', '', 'fa fa-link', CONVERT(bit, 1))
+,(2, 'Button Default', 'btn btn-default btn-sm', 'fa fa-square', CONVERT(bit, 0))
+,(3, 'Button Primary', 'btn btn-primary btn-sm', 'fa fa-square', CONVERT(bit, 0))
+,(4, 'Button Success', 'btn btn-success btn-sm', 'fa fa-square', CONVERT(bit, 0))
+,(5, 'Button Info', 'btn btn-info btn-sm', 'fa fa-square', CONVERT(bit, 0))
+,(6, 'Button Warning', 'btn btn-warning btn-sm', 'fa fa-square', CONVERT(bit, 0))
+,(7, 'Button Danger', 'btn btn-danger btn-sm', 'fa fa-square', CONVERT(bit, 0))
+) AS V(StyleValue, StyleLabel, StyleClass, StyleGlyph, StyleDefault)
