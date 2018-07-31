4325594f4dd1b014ea791e34c9eba3426063ba4c
Eitan Blumin <flatline972@hotmail.com>
2018-07-31 21:34:31 +0300

    #14 Implement porta.Navigation usage in side nav menu

---
+++ src/CrudePortalDB/Scripts/Script.PreDeployment.sql
@@ -0,0 +1,22 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+IF OBJECT_ID('portal.GetNavigationRecursive') IS NULL
+BEGIN
+	PRINT 'Creating stub for portal.GetNavigationRecursive...'
+	EXEC (N'CREATE FUNCTION [portal].[GetNavigationRecursive](@ParentNavId INT)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	RETURN N''''
+END');
+
+END
