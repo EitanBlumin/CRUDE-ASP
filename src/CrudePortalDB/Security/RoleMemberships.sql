cd13a9e2b942b6ec7d6a25634b120946561d5f8e
Eitan Blumin <flatline972@hotmail.com>
2019-04-07 19:50:04 +0300

    closed #13

--- src/CrudePortalDB/Security/RoleMemberships.sql
+++ src/CrudePortalDB/Security/RoleMemberships.sql
@@ -1,6 +1,12 @@
 ﻿ALTER ROLE [db_datawriter] ADD MEMBER [CrudeLogin];
 
-
 GO
 ALTER ROLE [db_datareader] ADD MEMBER [CrudeLogin];
 
+GO
+GRANT VIEW SERVER STATE TO [CrudeLogin];
+
+GO
+GRANT EXECUTE TO [CrudeLogin];
+
+GO
