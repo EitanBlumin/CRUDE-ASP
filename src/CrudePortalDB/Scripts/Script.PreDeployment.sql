60831c967ebebb45f82157ca5f5d2165b5a2239b
eitan.blumin <eitan@madeiradata.com>
2019-04-14 12:19:55 +0300

    Added pre-deployment script for backwards compatibility

---
+++ src/CrudePortalDB/Scripts/Script.PreDeployment.sql
@@ -0,0 +1,13 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('portal.DataViewField') AND name = 'FieldIdentifier')
+	EXEC (N'UPDATE portal.DataViewField SET FieldIdentifier = ''Field_'' + CONVERT(nvarchar, FieldID) WHERE FieldIdentifier IS NULL');
