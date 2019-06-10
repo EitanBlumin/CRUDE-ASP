b6bd6e25ab5c4e44060074c38dcaa3940dadf7fe
eitan.blumin <eitan@madeiradata.com>
2019-06-10 17:45:30 +0300

    added views for user-created views, fields and actions didn't implement their usage yet

---
+++ src/CrudePortalDB/Views/portal.UserDataViewActions.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [portal].[UserDataViewActions]
+WITH SCHEMABINDING
+AS
+SELECT [ActionID], [ViewID], [ActionLabel], [ParentActionID], [ActionTooltip], [ActionDescription], [ActionOrder], [RequireConfirmation], [OpenURLInNewWindow], [ActionExpression], [GlyphIcon], [IsPerRow], [CSSButton], [ActionType], [DataViewTitle]
+FROM [portal].[DataViewAction]
+WHERE [ViewID] > 0
